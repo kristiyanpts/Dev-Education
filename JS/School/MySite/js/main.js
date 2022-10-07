18ad3e0bc60bc8804800a101cdddcfc2a2394dcc
KrisDevBG <kristiqn3456@gmail.com>
2022-10-07 20:06:00 +0300

    UPDATE

--- JS/School/MySite/js/main.js
+++ JS/School/MySite/js/main.js
@@ -26,9 +26,68 @@ function CheckShit(elementId) {
             document.body.style.backgroundColor = alegedColor;
             break;
         case 'bscolor':
+            document.getElementById("settings").style.transition = "0.5s";
             document.getElementById("settings").style.backgroundColor = alegedColor;
+            var all = document.getElementsByTagName("input");
+            for (var i=0, max=all.length; i < max; i++) {
+                all[i].style.transition = "0.5s";
+                all[i].style.backgroundColor = alegedColor;
+            }
+            break;
+        case 'tcolor':
+            var all = document.getElementsByTagName("*");
+            for (var i=0, max=all.length; i < max; i++) {
+                all[i].style.transition = "0.5s";
+                all[i].style.color = alegedColor;
+            }
+            break;
+        case 'pcolor':
+            let progresses = document.getElementsByClassName("main-inner-progressbar");
+            progresses = Array.from(progresses);
+            for (let i = 0; i < progresses.length; i++) {
+                progresses[i].style.backgroundColor = alegedColor;
+            }
+            break;
+        case 'fperc':
+            document.getElementById("main-inner-progressbar-f").style.width = alegedColor || 0 + "%";
+            const obj1 = document.getElementById("main-inner-progressbar-f-t");
+            animateValue(obj1, parseInt(document.getElementById("main-inner-progressbar-t-t").innerHTML), alegedColor, 500);
+            break;
+        case 'sperc':
+            document.getElementById("main-inner-progressbar-s").style.width = alegedColor || 0 + "%";
+            const obj2 = document.getElementById("main-inner-progressbar-s-t");
+            animateValue(obj2, parseInt(document.getElementById("main-inner-progressbar-t-t").innerHTML), alegedColor, 500);
+            break;
+        case 'tperc':
+            document.getElementById("main-inner-progressbar-t").style.width = alegedColor || 0 + "%";
+            const obj = document.getElementById("main-inner-progressbar-t-t");
+            animateValue(obj, parseInt(document.getElementById("main-inner-progressbar-t-t").innerHTML), alegedColor, 500);
             break;
         default:
             break;
+    }
+}
+
+function animateValue(obj, start, end, duration) {
+    let startTimestamp = null;
+    const step = (timestamp) => {
+      if (!startTimestamp) startTimestamp = timestamp;
+      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+      obj.innerHTML = Math.floor(progress * (end - start) + start) + "%";
+      if (progress < 1) {
+        window.requestAnimationFrame(step);
+      }
+    };
+    window.requestAnimationFrame(step);
+}
+
+let settingsShown = false;
+function ToggleSettings() {
+    if (settingsShown) {
+        document.getElementById("settings").style.opacity = 0;
+        settingsShown = false;
+    } else {
+        document.getElementById("settings").style.opacity = 1;
+        settingsShown = true;
     }
 }
