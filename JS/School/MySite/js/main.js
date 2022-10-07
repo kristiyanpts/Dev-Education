7bd627b444b48c2d4820c79511bf72ab314d3de1
KrisDevBG <kristiqn3456@gmail.com>
2022-10-07 11:30:19 +0300

    School Project

---
+++ JS/School/MySite/js/main.js
@@ -0,0 +1,19 @@
+document.getElementById("my-work").addEventListener("click", function() {
+    window.open("https://github.com/KrisDevBG?tab=repositories");
+});
+
+document.getElementById("my-work-button").addEventListener("click", function() {
+    window.open("https://github.com/KrisDevBG?tab=repositories");
+});
+
+document.getElementById("home-page").addEventListener("click", function() {
+    window.location.replace("../home.html");
+});
+
+document.getElementById("about-page").addEventListener("click", function() {
+    window.location.replace("../about.html");
+});
+
+document.getElementById("contact-page").addEventListener("click", function() {
+    window.location.replace("../contact.html");
+});
