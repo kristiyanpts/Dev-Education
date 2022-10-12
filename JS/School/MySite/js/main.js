f5dd2b25f5d7acb294726096d38cb797a5238f6c
KrisDevBG <kristiqn3456@gmail.com>
2022-10-12 20:44:22 +0300

    Update

--- JS/School/MySite/js/main.js
+++ JS/School/MySite/js/main.js
@@ -106,3 +106,23 @@ function ToggleSettings() {
         settingsShown = true;
     }
 }
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.opacity = 1;
+  } else {
+    mybutton.style.opacity = 0;
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+window.scrollTo({top: 0, behavior: 'smooth'});
+}
