8742528216babfa228b1957b9bb17cf346e90ffa
KrisDevBG <kristiqn3456@gmail.com>
2022-11-11 14:08:09 +0200

    Exam shit

---
+++ JS/School/JS Exam 11/11/2022/Rectangle/rectangle.js
@@ -0,0 +1,10 @@
+function Calculate() {
+    let a = Number(document.getElementById("a").value);
+    let b = (document.getElementById("b").value);
+
+    if (!Number.isNaN(a) && !Number.isNaN(b)) {
+        let area = a * b;
+        let circ = (a * 2) + (b * 2);
+        document.getElementById("result").value = `Лице: ${area} | Обиколка: ${circ}`;
+    }
+}
