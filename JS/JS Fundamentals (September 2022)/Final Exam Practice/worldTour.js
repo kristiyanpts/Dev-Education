8b474cee9fca01fdb4b81d2edc1d328d178a084d
KrisDevBG <kristiqn3456@gmail.com>
2022-12-03 17:27:42 +0200

    this shit task is finally fixed

--- JS/JS Fundamentals (September 2022)/Final Exam Practice/worldTour.js
+++ JS/JS Fundamentals (September 2022)/Final Exam Practice/worldTour.js
@@ -1,40 +1,48 @@
 function Main(Input) {
-    let stops = Input.shift();
-    let commandInfo = Input.shift().split(":");
+  let stops = Input.shift();
+  let commandInfo = Input.shift().split(":");
 
-    while (commandInfo[0] !== "Travel") {
-        switch (commandInfo[0]) {
-            case "Add Stop":
-                let index = Number(commandInfo[1]);
-                let tempStop = commandInfo[2];
-                stops = stops.slice(0, index) + tempStop + stops.slice(index);
-                console.log(stops);
-                break;
-            case "Remove Stop":
-                let startIndex = Number(commandInfo[1]);
-                let endIndex = Number(commandInfo[2]);
-                stops = stops.slice(0, startIndex) + stops.slice(endIndex + 1);
-                console.log(stops);
-                break;
-            case "Switch":
-                while(stops.includes(commandInfo[1])){
-                    let splitted = stops.split(commandInfo[1]);
-                    stops = splitted.join(commandInfo[2]);
-                }
-                console.log(stops);
-                break;
-            default:
-                break;
+  while (commandInfo[0] !== "Travel") {
+    switch (commandInfo[0]) {
+      case "Add Stop":
+        let index = Number(commandInfo[1]);
+        if (index >= 0 && index < stops.length) {
+          let tempStop = commandInfo[2];
+          stops = stops.slice(0, index) + tempStop + stops.slice(index);
         }
-
-        commandInfo = Input.shift().split(':');
+        break;
+      case "Remove Stop":
+        let startIndex = Number(commandInfo[1]);
+        let endIndex = Number(commandInfo[2]);
+        if (
+          startIndex >= 0 &&
+          startIndex < stops.length &&
+          endIndex >= 0 &&
+          endIndex < stops.length &&
+          startIndex <= endIndex
+        ) {
+          stops = stops.slice(0, startIndex) + stops.slice(endIndex + 1);
+        }
+        break;
+      case "Switch":
+        let rgx = new RegExp(commandInfo[1], "g");
+        stops = stops.replace(rgx, commandInfo[2]);
+        break;
+      default:
+        break;
     }
 
-    console.log(`Ready for world tour! Planned stops: ${stops}`);
+    console.log(stops);
+    commandInfo = Input.shift().split(":");
+  }
+
+  console.log(`Ready for world tour! Planned stops: ${stops}`);
 }
 
-Main(["Hawai::Cyprys-Greece:Hawai:Hawai:Hawai",
-"Add Stop:7:Rome",
-"Remove Stop:11:16",
-"Switch:Hawai:Bulgaria",
-"Travel"])
+Main([
+  "Hawai::Cyprys-Greece:Hawai:Hawai:Hawai",
+  "Add Stop:7:Rome",
+  "Remove Stop:11:16",
+  "Switch:Hawai:Bulgaria",
+  "Travel",
+]);
