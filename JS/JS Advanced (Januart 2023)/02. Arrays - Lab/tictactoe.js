f788628ffc74cc48eb20ec39440baac479af540a
KrisDevBG <kristiqn3456@gmail.com>
2023-01-16 22:12:42 +0200

    softuni

--- JS/JS Advanced (Januart 2023)/02. Arrays - Lab/tictactoe.js
+++ JS/JS Advanced (Januart 2023)/02. Arrays - Lab/tictactoe.js
@@ -8,7 +8,11 @@ function Main(moves) {
   ];
 
   let currentPlayerMove = "X";
+  let playerWon = false;
   for (let i = 0; i < moves.length; i++) {
+    if (playerWon !== false) {
+      break;
+    }
     let move = moves[i].split(" ").map(Number);
     if (gameBoard[move[0]][move[1]] === false) {
       gameBoard[move[0]][move[1]] = currentPlayerMove;
@@ -16,11 +20,64 @@ function Main(moves) {
         ? (currentPlayerMove = "O")
         : (currentPlayerMove = "X");
     } else {
-      console.log(move);
+      for (const row of gameBoard) {
+        console.log(row.join("\t"));
+      }
       console.log("This place is already taken. Please choose another!");
     }
+    HasGameEnded();
   }
-  console.log(JSON.stringify(gameBoard));
+  playerWon !== false
+    ? console.log(`Player ${playerWon} wins!`)
+    : console.log("The game ended! Nobody wins :(");
+  for (const row of gameBoard) {
+    console.log(row.join("\t"));
+  }
+  function HasGameEnded() {
+    for (let row = 0; row < gameBoard.length; row++) {
+      for (let col = 0; col < gameBoard[row].length; col++) {
+        if (
+          gameBoard[row + 1] !== undefined &&
+          gameBoard[row][col] === gameBoard[row + 1][col] &&
+          gameBoard[row + 2] !== undefined &&
+          gameBoard[row][col] === gameBoard[row + 2][col]
+        ) {
+          playerWon = gameBoard[row][col];
+          break;
+        }
+        if (
+          gameBoard[row] !== undefined &&
+          gameBoard[row][col] === gameBoard[row][col + 1] &&
+          gameBoard[row][col + 2] !== undefined &&
+          gameBoard[row][col] === gameBoard[row][col + 2]
+        ) {
+          playerWon = gameBoard[row][col];
+          break;
+        }
+        if (
+          gameBoard[row + 1] !== undefined &&
+          gameBoard[row][col] === gameBoard[row + 1][col + 1] &&
+          gameBoard[row + 2] !== undefined &&
+          gameBoard[row][col] === gameBoard[row + 2][col + 2]
+        ) {
+          playerWon = gameBoard[row][col];
+          break;
+        }
+        if (
+          gameBoard[row + 1] !== undefined &&
+          gameBoard[row][col] === gameBoard[row + 1][col - 1] &&
+          gameBoard[row + 2] !== undefined &&
+          gameBoard[row][col] === gameBoard[row + 2][col - 2]
+        ) {
+          playerWon = gameBoard[row][col];
+          break;
+        }
+      }
+      if (playerWon !== false) {
+        break;
+      }
+    }
+  }
 }
 
 Main(["0 1", "0 0", "0 2", "2 0", "1 0", "1 2", "1 1", "2 1", "2 2", "0 0"]);
