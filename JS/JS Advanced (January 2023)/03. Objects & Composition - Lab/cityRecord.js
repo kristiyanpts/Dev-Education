6351ec0445ef550330acaff671be5270e494f822
Kristiyan Petsanov <kristiqn3456@gmail.com>
2023-01-17 20:21:21 +0200

    softuni

---
+++ JS/JS Advanced (January 2023)/03. Objects & Composition - Lab/cityRecord.js
@@ -0,0 +1,16 @@
+function Main(name, population, treasury) {
+  let city = {
+    name: name,
+    population: population,
+    treasury: treasury,
+  };
+  console.log(city);
+}
+
+Main(
+  "Tortuga",
+
+  7000,
+
+  15000
+);
