331a36a636206d8d467ff060ade2c7b44dfef2fc
Kristiyan Petsanov <kristiqn3456@gmail.com>
2023-03-02 13:14:24 +0200

    softuni

--- JS/JS Applications (February 2023)/03. Data and Authentication - Exercise/05.Fisher-Game/src/login.js
+++ JS/JS Applications (February 2023)/03. Data and Authentication - Exercise/05.Fisher-Game/src/login.js
@@ -1,1 +1,47 @@
-console.log('TODO:// Implement Login functionality');
+window.onload = function () {
+  if (localStorage.getItem("accessToken")) {
+    document.getElementById("user").style.display = "inline-block";
+    document.getElementById("guest").style.display = "none";
+    document.querySelector(".email span").textContent =
+      localStorage.getItem("email");
+  } else {
+    document.getElementById("user").style.display = "none";
+    document.getElementById("guest").style.display = "inline-block";
+  }
+
+  let form = document.querySelector("form");
+  form.addEventListener("submit", loginUser);
+};
+
+async function loginUser(event) {
+  event.preventDefault();
+  let notification = document.querySelector(".notification");
+  let formData = new FormData(event.target);
+  let { email, password } = Object.fromEntries(formData.entries());
+
+  const url = "http://localhost:3030/users/login";
+
+  await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((res) => {
+      if (res.ok == false) {
+        let err = res.json();
+        throw err;
+      }
+      return res.json();
+    })
+    .then((data) => {
+      localStorage.setItem("accessToken", data.accessToken);
+      localStorage.setItem("_id", data._id);
+      localStorage.setItem("email", data.email);
+      window.location.href = "index.html";
+    })
+    .catch((err) => {
+      notification.textContent = "Couldn't log you in.";
+    });
+}
--- JS/JS Applications (February 2023)/03. Data and Authentication - Exercise/05.Fisher-Game/src/register.js
+++ JS/JS Applications (February 2023)/03. Data and Authentication - Exercise/05.Fisher-Game/src/register.js
@@ -1,1 +1,52 @@
-console.log('TODO:// Implement Register functionality');
+window.onload = function () {
+  if (localStorage.getItem("accessToken")) {
+    document.getElementById("user").style.display = "inline-block";
+    document.getElementById("guest").style.display = "none";
+    document.querySelector(".email span").textContent =
+      localStorage.getItem("email");
+  } else {
+    document.getElementById("user").style.display = "none";
+    document.getElementById("guest").style.display = "inline-block";
+  }
+
+  let form = document.querySelector("form");
+  form.addEventListener("submit", registerUser);
+};
+
+async function registerUser(event) {
+  event.preventDefault();
+  let notification = document.querySelector(".notification");
+  let formData = new FormData(event.target);
+  let { email, password, rePass } = Object.fromEntries(formData.entries());
+  const url = "http://localhost:3030/users/register";
+  if (email == "" || password == "") {
+    return (notification.textContent = "All fields are required!");
+  }
+  if (password != rePass) {
+    return (notification.textContent = "Passwords do not match!");
+  }
+
+  await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((res) => {
+      if (res.ok == false) {
+        let err = res.json();
+        throw err;
+      }
+      return res.json();
+    })
+    .then((data) => {
+      localStorage.setItem("accessToken", data.accessToken);
+      localStorage.setItem("email", data.email);
+      localStorage.setItem("_id", data._id);
+      window.location.href = "index.html";
+    })
+    .catch((err) => {
+      notification.textContent = err.message;
+    });
+}
