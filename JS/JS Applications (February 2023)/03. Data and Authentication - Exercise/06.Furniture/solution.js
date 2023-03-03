f538d3f471f5b533e25c021811c7110389cb5187
Kristiyan Petsanov <kristiqn3456@gmail.com>
2023-03-03 11:57:18 +0200

    softuni

--- JS/JS Applications (February 2023)/03. Data and Authentication - Exercise/06.Furniture/solution.js
+++ JS/JS Applications (February 2023)/03. Data and Authentication - Exercise/06.Furniture/solution.js
@@ -1,4 +1,269 @@
-function solve() {
+function solve() {}
 
-  //TODO...
+let isLoggedIn = localStorage.getItem("accessToken") != null ? true : false;
+let currentWindow = window.location.pathname.split("/").pop();
+if (isLoggedIn && currentWindow == "homeLogged.html") {
+  let form = document.querySelector("form[action='']");
+  form.addEventListener("submit", createFurniture);
+  let logoutBtn = document.getElementById("logoutBtn");
+  logoutBtn.addEventListener("click", logoutUser);
+  let buttonBuy = Array.from(document.querySelectorAll("button"))[1];
+  buttonBuy.addEventListener("click", buyFurniture);
+  let buttonShowSales = Array.from(document.querySelectorAll("button"))[2];
+  buttonShowSales.addEventListener("click", showSales);
+}
+if (currentWindow == "login.html") {
+  let loginForm = document.querySelector("form[action='/login']");
+  loginForm.addEventListener("submit", loginUser);
+  let registerForm = document.querySelector("form[action='/register']");
+  registerForm.addEventListener("submit", registerUser);
+}
+if (currentWindow == "home.html" || currentWindow == "homeLogged.html") {
+  getFurniture();
+}
+
+async function registerUser(event) {
+  event.preventDefault();
+  let formData = new FormData(event.target);
+  let { email, password, rePass } = Object.fromEntries(formData.entries());
+  const url = "http://localhost:3030/users/register";
+  if (email == "" || password == "") {
+    return alert("All fields are required!");
+  }
+  if (password != rePass) {
+    return alert("Passwords do not match!");
+  }
+
+  await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then(async (res) => {
+      if (res.ok == false) {
+        let err = await res.json();
+        throw err;
+      }
+      return await res.json();
+    })
+    .then((data) => {
+      localStorage.setItem("accessToken", data.accessToken);
+      localStorage.setItem("email", data.email);
+      localStorage.setItem("id", data._id);
+      window.location.href = "homeLogged.html";
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
+}
+
+async function logoutUser() {
+  const url = "http://localhost:3030/users/logout";
+  await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Authorization": localStorage.getItem("accessToken"),
+    },
+  });
+  localStorage.clear();
+  window.location.href = "home.html";
+}
+
+async function loginUser(event) {
+  event.preventDefault();
+  let formData = new FormData(event.target);
+  let { email, password } = Object.fromEntries(formData.entries());
+
+  const url = "http://localhost:3030/users/login";
+
+  await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then(async (res) => {
+      if (res.ok == false) {
+        let err = await res.json();
+        throw err;
+      }
+      return await res.json();
+    })
+    .then((data) => {
+      localStorage.setItem("accessToken", data.accessToken);
+      localStorage.setItem("id", data._id);
+      localStorage.setItem("email", data.email);
+      window.location.href = "homeLogged.html";
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
+}
+
+async function createFurniture(event) {
+  event.preventDefault();
+  let formData = new FormData(event.target);
+  let { name, price, factor, img } = Object.fromEntries(formData.entries());
+  if (name === "" || price === "" || factor === "" || img === "") {
+    return;
+  }
+  const url = "http://localhost:3030/data/furniture";
+  await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Authorization": localStorage.getItem("accessToken"),
+    },
+    body: JSON.stringify({
+      name,
+      price,
+      factor,
+      img,
+    }),
+  })
+    .then(async (res) => {
+      if (res.ok == false) {
+        let err = await res.json();
+        throw err;
+      }
+      return await res.json();
+    })
+    .then((data) => {
+      getFurniture();
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
+}
+
+async function getFurniture() {
+  let isDisabled = localStorage.getItem("accessToken") != null ? false : true;
+  let tbody = document.querySelector(".table tbody");
+  tbody.innerHTML = "";
+  const url = "http://localhost:3030/data/furniture";
+  await fetch(url)
+    .then(async (response) => {
+      if (response.ok == false) {
+        let err = await response.json();
+        throw err;
+      }
+      return await response.json();
+    })
+    .then((data) => {
+      data.forEach((f) => {
+        let tr = document.createElement("tr");
+        tr.innerHTML = `
+          <td>
+            <img src="${f.img}">
+          </td>
+          <td>
+              <p>${f.name}</p>
+          </td>
+          <td>
+              <p>${f.price}</p>
+          </td>
+          <td>
+              <p>${f.factor}</p>
+          </td>
+          <td>
+              <input data-id="${f._id}" type="checkbox" ${
+          isDisabled ? "disabled" : ""
+        }/>
+          </td>
+        `;
+        tbody.appendChild(tr);
+      });
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
+}
+
+async function buyFurniture() {
+  let checkBoxes = document.querySelectorAll("input[type=checkbox]:checked");
+
+  if (checkBoxes.length == 0) {
+    return alert("Please select furniture!");
+  }
+
+  checkBoxes.forEach(async (f) => {
+    let id = f.getAttribute("data-id");
+    const furnitureUrl = `http://localhost:3030/data/furniture/${id}`;
+
+    await fetch(furnitureUrl)
+      .then(async (response) => {
+        if (response.ok == false) {
+          let err = await response.json();
+          throw err;
+        }
+        return await response.json();
+      })
+      .then((data) => {
+        orderProduct(data);
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  });
+}
+
+async function orderProduct(data) {
+  const ordersUrl = `http://localhost:3030/data/orders`;
+
+  await fetch(ordersUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Authorization": localStorage.getItem("accessToken"),
+    },
+    body: JSON.stringify(data),
+  })
+    .then(async (res) => {
+      if (res.ok == false) {
+        let err = await res.json();
+        throw err;
+      }
+      return await res.json();
+    })
+    .then((data) => {
+      console.log("Successfully ordered product");
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
+}
+
+async function showSales() {
+  const url = `http://localhost:3030/data/orders?where=_ownerId%3D"${localStorage.getItem(
+    "id"
+  )}"`;
+  let productsBought = [];
+  let totalPrice = 0;
+  await fetch(url)
+    .then(async (res) => {
+      if (res.ok == false) {
+        let err = await res.json();
+        throw err;
+      }
+      return await res.json();
+    })
+    .then((data) => {
+      data.forEach((f) => {
+        if (!productsBought.includes(f.name)) {
+          productsBought.push(f.name);
+        }
+        totalPrice += Number(f.price);
+      });
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
+
+  let paras = Array.from(document.querySelectorAll(".orders p span"));
+  paras[0].textContent = productsBought.join(", ");
+  paras[1].textContent = totalPrice + " $";
 }
--- JS/JS Applications (February 2023)/03. Data and Authentication - Exercise/06.Furniture/tests/e2e.test.js
+++ JS/JS Applications (February 2023)/03. Data and Authentication - Exercise/06.Furniture/tests/e2e.test.js
@@ -1,7 +1,8 @@
-const { chromium } = require('playwright-chromium');
-const { expect } = require('chai');
+const { chromium } = require("playwright-chromium");
+const { expect } = require("chai");
 
-const host = 'http://localhost:3000'; // Application host (NOT service host - that can be anything)
+const host =
+  "http://127.0.0.1:5500/JS/JS%20Applications%20(February%202023)/03.%20Data%20and%20Authentication%20-%20Exercise/06.Furniture/"; // Application host (NOT service host - that can be anything)
 const interval = 300;
 const DEBUG = false;
 const slowMo = 500;
@@ -9,53 +10,53 @@ const slowMo = 500;
 const mockData = {
   users: [
     {
-      _id: '0001',
-      email: 'peter@abv.bg',
-      password: '123456',
-      accessToken: 'AAAA',
+      _id: "0001",
+      email: "peter@abv.bg",
+      password: "123456",
+      accessToken: "AAAA",
     },
     {
-      _id: '0002',
-      email: 'john@abv.bg',
-      password: '123456',
-      accessToken: 'BBBB',
+      _id: "0002",
+      email: "john@abv.bg",
+      password: "123456",
+      accessToken: "BBBB",
     },
   ],
   catalog: [
     {
-      _ownerId: '0001',
-      name: 'Chair',
-      price: '23',
-      factor: '12',
-      img: 'image1',
-      _id: '1001',
+      _ownerId: "0001",
+      name: "Chair",
+      price: "23",
+      factor: "12",
+      img: "image1",
+      _id: "1001",
     },
     {
-      _ownerId: '0002',
-      name: 'Table',
-      price: '50',
-      factor: '2',
-      img: 'image2',
-      _id: '1002',
+      _ownerId: "0002",
+      name: "Table",
+      price: "50",
+      factor: "2",
+      img: "image2",
+      _id: "1002",
     },
   ],
   orders: [
     {
-      _ownerId: '0001',
+      _ownerId: "0001",
       boughtFurniture: [],
       totalSum: 0,
-      _id: '1001',
+      _id: "1001",
     },
   ],
 };
 
 const endpoints = {
-  register: '/users/register',
-  login: '/users/login',
-  logout: '/users/logout',
-  catalog: '/data/furniture',
-  create: '/data/furniture',
-  orders: '/data/orders',
+  register: "/users/register",
+  login: "/users/login",
+  logout: "/users/logout",
+  catalog: "/data/furniture",
+  create: "/data/furniture",
+  orders: "/data/orders",
   owner: (userId) => `/data/orders?where=_ownerId%3D${userId}`,
 };
 
@@ -63,14 +64,14 @@ let browser;
 let context;
 let page;
 
-describe('E2E tests', function () {
+describe("E2E tests", function () {
   // Setup
   this.timeout(DEBUG ? 120000 : 7000);
   before(
     async () =>
-    (browser = await chromium.launch(
-      DEBUG ? { headless: false, slowMo } : {}
-    ))
+      (browser = await chromium.launch(
+        DEBUG ? { headless: false, slowMo } : {}
+      ))
   );
   after(async () => await browser.close());
   beforeEach(async () => {
@@ -83,40 +84,40 @@ describe('E2E tests', function () {
     await context.close();
   });
   // Test proper
-  describe('Authentication', () => {
-    it('register does not work with empty fields', async () => {
+  describe("Authentication", () => {
+    it("register does not work with empty fields", async () => {
       const { post } = await handle(endpoints.register);
       const isCalled = post().isHandled;
 
       await page.goto(host);
-      await page.waitForSelector('#guest');
+      await page.waitForSelector("#guest");
 
-      await page.click('nav >> text=Login');
-      await page.waitForSelector('#register-form');
+      await page.click("nav >> text=Login");
+      await page.waitForSelector("#register-form");
 
-      await page.click('#register-form >> text=Register');
+      await page.click("#register-form >> text=Register");
 
       expect(isCalled()).to.be.false;
     });
 
-    it('register makes correct API call', async () => {
+    it("register makes correct API call", async () => {
       const data = mockData.users[0];
       const { post } = await handle(endpoints.register);
       const { onRequest } = post(data);
 
       await page.goto(host);
-      await page.waitForSelector('#guest');
+      await page.waitForSelector("#guest");
 
-      await page.click('nav >> text=Login');
+      await page.click("nav >> text=Login");
 
-      await page.waitForSelector('#register-form');
+      await page.waitForSelector("#register-form");
       await page.fill('#register-form >> [name="email"]', data.email);
       await page.fill('#register-form >> [name="password"]', data.password);
       await page.fill('#register-form >> [name="rePass"]', data.password);
 
       const [request] = await Promise.all([
         onRequest(),
-        page.click('#register-form >> text=Register'),
+        page.click("#register-form >> text=Register"),
       ]);
 
       const postData = JSON.parse(request.postData());
@@ -125,22 +126,22 @@ describe('E2E tests', function () {
       expect(postData.password).to.equal(data.password);
     });
 
-    it('login makes correct API call', async () => {
+    it("login makes correct API call", async () => {
       const data = mockData.users[0];
       const { post } = await handle(endpoints.login);
       const { onRequest } = post(data);
 
       await page.goto(host);
-      await page.waitForSelector('#guest');
+      await page.waitForSelector("#guest");
 
-      await page.click('nav >> text=Login');
-      await page.waitForSelector('#login-form');
+      await page.click("nav >> text=Login");
+      await page.waitForSelector("#login-form");
       await page.fill('#login-form [name="email"]', data.email);
       await page.fill('#login-form [name="password"]', data.password);
 
       const [request] = await Promise.all([
         onRequest(),
-        page.click('#login-form >> text=Login'),
+        page.click("#login-form >> text=Login"),
       ]);
 
       const postData = JSON.parse(request.postData());
@@ -148,75 +149,76 @@ describe('E2E tests', function () {
       expect(postData.password).to.equal(data.password);
     });
 
-    it('Logout makes correct API call', async () => {
+    it("Logout makes correct API call", async () => {
       const data = mockData.users[0];
       const { post } = await handle(endpoints.login);
       const { get } = await handle(endpoints.logout);
       const { onResponse } = post(data);
-      const { onRequest } = get('', { json: false, status: 204 });
+      const { onRequest } = get("", { json: false, status: 204 });
 
       await page.goto(host);
-      await page.waitForSelector('#guest');
-      await page.click('text=Login');
+      await page.waitForSelector("#guest");
+      await page.click("text=Login");
 
-      await page.waitForSelector('#login-form');
+      await page.waitForSelector("#login-form");
       await page.fill('#login-form [name="email"]', data.email);
       await page.fill('#login-form [name="password"]', data.password);
 
       await Promise.all([
         onResponse(),
-        page.click('#login-form >> text=Login'),
+        page.click("#login-form >> text=Login"),
       ]);
 
-      await page.waitForSelector('nav');
+      await page.waitForSelector("nav");
 
       const [request] = await Promise.all([
         onRequest(),
-        page.click('nav >> text=Logout'),
+        page.click("nav >> text=Logout"),
       ]);
-      expect(request.method()).to.equal('GET');
+      expect(request.method()).to.equal("GET");
     });
   });
 
-  describe('Navigation bar', () => {
-    it('guest user should see correct navigation', async () => {
+  describe("Navigation bar", () => {
+    it("guest user should see correct navigation", async () => {
       await page.goto(host);
-      await page.waitForSelector('#guest');
+      await page.waitForSelector("#guest");
 
-      expect(await page.isVisible('#guest')).to.be.true;
+      expect(await page.isVisible("#guest")).to.be.true;
 
-      expect(await page.isVisible('#user')).to.be.false;
+      expect(await page.isVisible("#user")).to.be.false;
     });
 
-    it('logged user should see correct navigation', async () => {
+    it("logged user should see correct navigation", async () => {
       // Login user
       const data = mockData.users[0];
       await page.goto(host);
-      await page.waitForSelector('#guest');
+      await page.waitForSelector("#guest");
 
-      await page.click('nav >> text=Login');
+      await page.click("nav >> text=Login");
 
-      await page.waitForSelector('#login-form');
+      await page.waitForSelector("#login-form");
       await page.fill('#login-form [name="email"]', data.email);
       await page.fill('#login-form [name="password"]', data.password);
 
-      page.click("#login-form >> text=Login"), await page.waitForTimeout(interval);
+      page.click("#login-form >> text=Login"),
+        await page.waitForTimeout(interval);
 
       //Test for navigation
-      expect(await page.isVisible('#guest')).to.be.false;
+      expect(await page.isVisible("#guest")).to.be.false;
 
-      expect(await page.isVisible('#user')).to.be.true;
+      expect(await page.isVisible("#user")).to.be.true;
     });
   });
 
-  describe('Catalog', () => {
-    it('view furniture', async () => {
+  describe("Catalog", () => {
+    it("view furniture", async () => {
       const data = mockData.catalog;
       const { get } = await handle(endpoints.catalog);
       get(data);
       await page.goto(host);
 
-      await page.waitForSelector('.table');
+      await page.waitForSelector(".table");
 
       const furniture = await page.$$eval(`.table tbody tr`, (t) =>
         t.map((s) => s)
@@ -225,46 +227,46 @@ describe('E2E tests', function () {
       expect(furniture.length).to.be.equal(data.length);
     });
 
-    it('nothing bought', async () => {
+    it("nothing bought", async () => {
       const data = mockData.catalog;
       const { get } = await handle(endpoints.catalog);
       get(data);
       await page.goto(host);
       const user = mockData.users[0];
-      await page.waitForSelector('#guest');
-      await page.click('text=Login');
+      await page.waitForSelector("#guest");
+      await page.click("text=Login");
 
-      await page.waitForSelector('#login-form');
+      await page.waitForSelector("#login-form");
       await page.fill('#login-form [name="email"]', user.email);
       await page.fill('#login-form [name="password"]', user.password);
-      await page.click('#login-form >> text=Login');
+      await page.click("#login-form >> text=Login");
 
       const { get2 } = await handle(endpoints.owner(data._id));
       get2(data);
 
-      await page.click('#show-orders-btn');
-      await page.waitForSelector('.orders');
+      await page.click("#show-orders-btn");
+      await page.waitForSelector(".orders");
       const furniture = await page.$$eval(`.orders p span`, (t) =>
         t.map((s) => s.textContent)
       );
-      expect(furniture[0]).to.be.contains('Nothing bought yet!');
-      expect(furniture[1]).to.be.contains('0 $');
+      expect(furniture[0]).to.be.contains("Nothing bought yet!");
+      expect(furniture[1]).to.be.contains("0 $");
     });
 
-    it('bought fruniture', async () => {
+    it("bought fruniture", async () => {
       const data = mockData.catalog;
       const { get } = await handle(endpoints.catalog);
       get(data);
       await page.goto(host);
       const user = mockData.users[0];
-      await page.waitForSelector('#guest');
-      await page.click('text=Login');
+      await page.waitForSelector("#guest");
+      await page.click("text=Login");
 
-      await page.waitForSelector('#login-form');
+      await page.waitForSelector("#login-form");
       await page.fill('#login-form [name="email"]', user.email);
       await page.fill('#login-form [name="password"]', user.password);
-      await page.click('#login-form >> text=Login');
-      await page.waitForSelector('.table');
+      await page.click("#login-form >> text=Login");
+      await page.waitForSelector(".table");
 
       await page.click(`.table tbody tr input[type="checkbox"]`);
 
@@ -275,42 +277,42 @@ describe('E2E tests', function () {
     });
   });
 
-  describe('CRUD', () => {
+  describe("CRUD", () => {
     // Login user
     const loginUser = async () => {
       const data = mockData.users[0];
       await page.goto(host);
       await page.waitForTimeout(interval);
-      await page.click('text=Login');
+      await page.click("text=Login");
       await page.waitForTimeout(interval);
-      await page.waitForSelector('#login-form');
+      await page.waitForSelector("#login-form");
       await page.fill('#login-form [name="email"]', data.email);
       await page.fill('#login-form [name="password"]', data.password);
-      await page.click('#login-form >> text=Login');
+      await page.click("#login-form >> text=Login");
       await page.waitForTimeout(interval);
     };
 
-    it('create does NOT work with empty fields', async () => {
+    it("create does NOT work with empty fields", async () => {
       await loginUser();
       const { post } = await handle(endpoints.create);
       const isCalled = post().isHandled;
 
-      await page.click('text=Create Product');
-      await page.waitForSelector('#create-form');
+      await page.click("text=Create Product");
+      await page.waitForSelector("#create-form");
 
-      page.click('#create-form >> text=Create');
+      page.click("#create-form >> text=Create");
 
       expect(isCalled()).to.be.false;
     });
 
-    it('create makes correct API call for logged in user', async () => {
+    it("create makes correct API call for logged in user", async () => {
       await loginUser();
       const data = mockData.catalog[0];
       const { post } = await handle(endpoints.create);
       const { onRequest } = post();
 
-      await page.click('text=Create Product');
-      await page.waitForSelector('#create-form');
+      await page.click("text=Create Product");
+      await page.waitForSelector("#create-form");
 
       await page.fill('#create-form >> [name="name"]', data.name);
       await page.fill('#create-form >> [name="price"]', data.price);
@@ -319,7 +321,7 @@ describe('E2E tests', function () {
 
       const [request] = await Promise.all([
         onRequest(),
-        page.click('#create-form >> text=Create'),
+        page.click("#create-form >> text=Create"),
       ]);
 
       const postData = JSON.parse(request.postData());
@@ -336,27 +338,27 @@ async function setupContext(context) {
   await handleContext(context, endpoints.login, { post: mockData.users[0] });
   await handleContext(context, endpoints.register, { post: mockData.users[0] });
   await handleContext(context, endpoints.logout, {
-    get: (h) => h('', { json: false, status: 204 }),
+    get: (h) => h("", { json: false, status: 204 }),
   });
 
   // Catalog and Details
   await handleContext(context, endpoints.catalog, { get: mockData.catalog });
   await handleContext(context, endpoints.orders, { get: mockData.orders });
-  await handleContext(context, endpoints.owner('1001'), {
+  await handleContext(context, endpoints.owner("1001"), {
     get: mockData.orders[0],
   });
-  await handleContext(context, endpoints.details('1001'), {
+  await handleContext(context, endpoints.details("1001"), {
     get: mockData.catalog[0],
   });
-  await handleContext(context, endpoints.details('1002'), {
+  await handleContext(context, endpoints.details("1002"), {
     get: mockData.catalog[1],
   });
-  await handleContext(context, endpoints.details('1003'), {
+  await handleContext(context, endpoints.details("1003"), {
     get: mockData.catalog[2],
   });
 
   await handleContext(
-    endpoints.orders('0001'),
+    endpoints.orders("0001"),
     { get: mockData.catalog.slice(0, 2) },
     context
   );
@@ -366,7 +368,7 @@ async function setupContext(context) {
     (url) => url.href.slice(0, host.length) != host,
     (route) => {
       if (DEBUG) {
-        console.log('Preventing external call to ' + route.request().url());
+        console.log("Preventing external call to " + route.request().url());
       }
       route.abort();
     }
@@ -384,20 +386,20 @@ function handleContext(context, match, handlers) {
 async function handleRaw(match, handlers) {
   const methodHandlers = {};
   const result = {
-    get: (returns, options) => request('GET', returns, options),
-    get2: (returns, options) => request('GET', returns, options),
-    post: (returns, options) => request('POST', returns, options),
-    put: (returns, options) => request('PUT', returns, options),
-    patch: (returns, options) => request('PATCH', returns, options),
-    del: (returns, options) => request('DELETE', returns, options),
-    delete: (returns, options) => request('DELETE', returns, options),
+    get: (returns, options) => request("GET", returns, options),
+    get2: (returns, options) => request("GET", returns, options),
+    post: (returns, options) => request("POST", returns, options),
+    put: (returns, options) => request("PUT", returns, options),
+    patch: (returns, options) => request("PATCH", returns, options),
+    del: (returns, options) => request("DELETE", returns, options),
+    delete: (returns, options) => request("DELETE", returns, options),
   };
 
   const context = this;
 
   await context.route(urlPredicate, (route, request) => {
     if (DEBUG) {
-      console.log('>>>', request.method(), request.url());
+      console.log(">>>", request.method(), request.url());
     }
 
     const handler = methodHandlers[request.method().toLowerCase()];
@@ -410,7 +412,7 @@ async function handleRaw(match, handlers) {
 
   if (handlers) {
     for (let method in handlers) {
-      if (typeof handlers[method] == 'function') {
+      if (typeof handlers[method] == "function") {
         handlers[method](result[method]);
       } else {
         result[method](handlers[method]);
@@ -454,10 +456,10 @@ function respond(data, options = {}) {
   );
 
   const headers = {
-    'Access-Control-Allow-Origin': '*',
+    "Access-Control-Allow-Origin": "*",
   };
   if (options.json) {
-    headers['Content-Type'] = 'application/json';
+    headers["Content-Type"] = "application/json";
     data = JSON.stringify(data);
   }
 
