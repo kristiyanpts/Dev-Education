78381e7f935f96c79e14794b5cce3e2c0ecb99cb
Kristiyan Petsanov <kristiqn3456@gmail.com>
2023-03-31 08:02:37 +0300

    exam prep

--- JS/JS Applications (February 2023)/Exam Preparation/Sole Mates/src/views/dashboard.js
+++ JS/JS Applications (February 2023)/Exam Preparation/Sole Mates/src/views/dashboard.js
@@ -1,28 +1,24 @@
 import { getAllShoes } from "../api/soles.js";
 import { html } from "../lib.js";
-import { getUserData } from "../utils.js";
 
-let dashboardTemplate = (soles, user) => html`<section id="dashboard">
+let dashboardTemplate = (soles) => html`<section id="dashboard">
   <h2>Collectibles</h2>
   ${soles.length > 0
     ? html`<ul class="card-wrapper">
-        ${soles.map((e) => soleTemplate(e, user))}
+        ${soles.map(soleTemplate)}
       </ul>`
     : html`<h2>There are no items added yet.</h2>`}
 </section> `;
 
-let soleTemplate = (sole, user) => html`<li class="card">
+let soleTemplate = (sole) => html`<li class="card">
   <img src=${sole.imageUrl} alt="eminem" />
   <p><strong>Brand: </strong><span class="brand">${sole.brand}</span></p>
   <p><strong>Model: </strong><span class="model">${sole.model}</span></p>
   <p><strong>Value:</strong><span class="value">${sole.value}</span>$</p>
-  ${user
-    ? html`<a class="details-btn" href="/details/${sole._id}">Details</a>`
-    : null}
+  <a class="details-btn" href="/details/${sole._id}">Details</a>
 </li>`;
 
 export async function showDashboard(ctx) {
   let shoes = await getAllShoes();
-  let user = getUserData();
-  ctx.render(dashboardTemplate(shoes, user));
+  ctx.render(dashboardTemplate(shoes));
 }
