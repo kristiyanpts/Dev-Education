6549dfe1bd9e7d59ab6d106ea5599d4353ab557f
Kristiyan Petsanov <kristiqn3456@gmail.com>
2023-05-14 19:13:01 +0300

    backend

--- JS/JS Back-End (May 2023)/01. Intro to Node.js/handlers/home.js
+++ JS/JS Back-End (May 2023)/01. Intro to Node.js/handlers/home.js
@@ -1,22 +1,7 @@
 const url = require("url");
 const fs = require("fs");
 const path = require("path");
-const cats = [
-  {
-    name: "Shopito",
-    description: "Mnogo qka kotka",
-    image:
-      "https://m.netinfo.bg/media/images/50180/50180702/1250-666-porodista-kotka.jpg",
-    breed: "Razgonena",
-  },
-  {
-    name: "Shopito",
-    description: "Mnogo tupa kotka",
-    image:
-      "https://m.netinfo.bg/media/images/50180/50180702/1250-666-porodista-kotka.jpg",
-    breed: "Ne Razgonena",
-  },
-];
+const cats = require("../data/cats");
 
 module.exports = (req, res) => {
   const pathname = url.parse(req.url).pathname;
@@ -35,6 +20,28 @@ module.exports = (req, res) => {
         res.end();
         return;
       }
+
+      let modifiedCats = cats.map(
+        (cat) => `<li>
+          <img
+            src="${path.join("../content/images/" + cat.image)}"
+            alt="${cat.name}"
+          />
+          <h3>${cat.name}</h3>
+          <p><span>Breed: </span>${cat.breed}</p>
+          <p><span>Description: </span>${cat.description}</p>
+          <ul class="buttons">
+            <li class="btn edit"><a href="/cats-edit/${
+              cat.id
+            }">Change Info</a></li>
+            <li class="btn delete"><a href="/cats-find-new-home/${
+              cat.id
+            }">New Home</a></li>
+          </ul>
+        </li>`
+      );
+
+      homePage = homePage.toString().replace("{{cats}}", modifiedCats);
 
       res.writeHead(200, {
         "Content-Type": "text/html",
