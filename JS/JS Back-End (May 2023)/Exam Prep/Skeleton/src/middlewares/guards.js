46c05934ace1ebc888084dabd4fee066dd24d26e
Kristiyan Petsanov <kristiqn3456@gmail.com>
2023-06-10 22:28:03 +0300

    клкл

--- JS/JS Back-End (May 2023)/Exam Prep/Skeleton/src/middlewares/guards.js
+++ JS/JS Back-End (May 2023)/Exam Prep/Skeleton/src/middlewares/guards.js
@@ -1,13 +1,13 @@
 function hasUser() {
   return (req, res, next) => {
-    if (req.user) next();
+    if (req.user) return next();
     res.redirect("/auth/login");
   };
 }
 
 function isGuest() {
   return (req, res, next) => {
-    if (req.user) res.redirect("/"); // TIDO: Check assignment for correct redirect
+    if (req.user) return res.redirect("/"); // TIDO: Check assignment for correct redirect
     next();
   };
 }
