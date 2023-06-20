225cfbc5a83e9e70b0314b206f823ba8d003e448
Kristiyan Petsanov <kristiqn3456@gmail.com>
2023-06-20 22:04:42 +0300

    ddd

--- JS/06. Angular (June 2023)/01. Intro to Angular and TypeScript/Basic-App/src/app/app.module.ts
+++ JS/06. Angular (June 2023)/01. Intro to Angular and TypeScript/Basic-App/src/app/app.module.ts
@@ -3,18 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { XyzComponent } from './xyz/xyz.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    XyzComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
